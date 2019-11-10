3b07e299ddd6bf52a0ce7fb42a736c1083b82b53
Lewis Bell <lb16618@bc4login3.bc4.acrc.priv>
2019-11-10 12:26:20 +0000

    Add Slurm submission file

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --partition gpu
+#SBATCH --time 0-05:00
+#SBATCH --mem 64GB
+#SBATCH --gres gpu:1
+
+# get rid of any modules already loaded
+module purge
+# load in the module dependencies for this script
+module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
+
+python3 src/main.py
