b29098df357c2d493be607a0f03bf4e66e0819c3
Lewis Bell <lewisbell999@gmail.com>
2019-11-10 11:35:13 +0000

    PEP-8 fixes

--- src/cnn.py
+++ src/cnn.py
@@ -8,52 +8,53 @@ from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
+
 class ImageShape(NamedTuple):
     height: int
     width: int
     channels: int
 
+
 class CNN(nn.Module):
     def __init__(self, height: int, width: int, channels: int, class_count: int):
         super().__init__()
-        self.input_shape = ImageShape(height=height, width=width, channels=channels)
+        self.input_shape = ImageShape(
+            height=height, width=width, channels=channels)
         self.class_count = class_count
 
         self.conv1 = nn.Conv2d(
             in_channels=self.input_shape.channels,
             out_channels=32,
-            kernel_size=(3,3),
-            stride=(2,2)
+            kernel_size=(3, 3),
+            stride=(2, 2)
         )
         self.initialise_layer(self.conv1)
-        self.batch1=nn.BatchNorm2d(self.conv1.out_channels)
+        self.batch1 = nn.BatchNorm2d(self.conv1.out_channels)
         self.conv2 = nn.Conv2d(
             in_channels=self.input_shape.channels,
             out_channels=32,
-            kernel_size=(3,3),
-            stride=(2,2)
+            kernel_size=(3, 3),
+            stride=(2, 2)
         )
         self.initialise_layer(self.conv2)
-        self.batch2=nn.BatchNorm2d(self.conv2.out_channels)
-        self.pool1=nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv3=nn.Conv2d(
+        self.batch2 = nn.BatchNorm2d(self.conv2.out_channels)
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv3 = nn.Conv2d(
             in_channels=self.pool1.out_channels,
-            out_channels = 64,
-            kernel_size=(3,3),
-            stride=(2,2))
+            out_channels=64,
+            kernel_size=(3, 3),
+            stride=(2, 2))
         self.initialise_layer(self.conv3)
         self.batch3 = nn.BatchNorm2d(self.conv3.out_channels)
         self.conv4 = nn.Conv2d(
             in_channels=self.pool1.out_channels,
-            out_channels = 64,
-            kernel_size=(3,3),
-            stride=(2,2))
+            out_channels=64,
+            kernel_size=(3, 3),
+            stride=(2, 2))
         self.initialise_layer(self.conv4)
         self.batch4 = nn.BatchNorm2d(self.conv3.out_channels)
         self.fc1 = nn.Linear(1024, 10)
         self.initialise_layer(self.fc1)
-
-
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         x = F.relu(self.batch1(self.conv1(images)))
