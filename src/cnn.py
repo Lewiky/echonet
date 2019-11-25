e59c696e70bd4f86534f54bea8452c4bf2f219c5
Lewis Bell <lewisbell999@gmail.com>
2019-11-25 11:53:04 +0000

    Tidy up CNN forward

--- src/cnn.py
+++ src/cnn.py
@@ -63,27 +63,13 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(1024, 10)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(images)
-        x = self.batch1(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.conv2(x)
-        x = self.batch2(x)
-        x = F.relu(x)
-        x = self.pool1(x)
-        x = self.conv3(x)
-        x = self.batch3(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.conv4(x)
-        x = self.batch4(x)
-        x = F.relu(x)
+        x = F.relu(self.batch1(self.conv1(images)))
+        x = F.relu(self.batch2(self.conv2(self.dropout(x))))
+        x = F.relu(self.batch3(self.conv3(self.pool1(x))))
+        x = F.relu(self.batch4(self.conv4(self.dropout(x))))
         x = torch.flatten(x, start_dim=1)
-        x = self.dropout(x)
-        x = self.fc1(x)
-        x = F.sigmoid(x)
-        x = self.fc2(x)
-        return F.softmax(x)
+        x = torch.sigmoid(self.fc1(self.dropout(x)))
+        return F.softmax(self.fc2(x))
 
     @staticmethod
     def initialise_layer(layer):
