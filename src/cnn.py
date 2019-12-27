3f2dae171912525aa31ea8d9249f58b224c184d9
Jay Lees <jay.lees@me.com>
2019-12-27 20:08:53 +0000

    expand function application

--- src/cnn.py
+++ src/cnn.py
@@ -87,14 +87,35 @@ class CNN(nn.Module):
         print(f"Number of params: {params}")
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        x = self.batch1(F.relu(self.conv1(images)))
-        x = self.batch2(self.dropout(self.pool1(F.relu(self.conv2(x)))))
-        x = self.batch3(F.relu(self.conv3(x)))
-        x = self.batch4(self.dropout(F.relu(self.conv4(x))))
-        #x = self.pool2(x)
+        x = self.conv1(images)
+        x = F.relu(x)
+        x = self.batch1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        x = self.dropout(x)
+        x = self.batch2(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.batch3(x)
+
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.batch4(x)
+
         x = torch.flatten(x, start_dim=1)
-        x = self.dropout(torch.sigmoid(self.fc1(x)))
-        return F.softmax(self.fc2(x), dim=-1)
+
+        x = self.fc1(x)
+        x = torch.sigmoid(x)
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+        x = F.softmax(x, dim=-1)
+
+        return x 
 
     @staticmethod
     def initialise_layer(layer):
