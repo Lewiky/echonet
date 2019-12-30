ebebe0bb75c835afa637aac518fb089248db66e5
Jay Lees <jay.lees@me.com>
2019-12-30 17:53:31 +0000

    Use no bias and set softmax to variable

--- src/cnn.py
+++ src/cnn.py
@@ -31,7 +31,8 @@ class CNN(nn.Module):
             out_channels=32,
             kernel_size=(3, 3),
             stride=self.stride,
-            padding=(43,21)
+            padding=(43,21),
+            bias=False
         )
         self.initialise_layer(self.conv1)
         self.batch1 = nn.BatchNorm2d(self.conv1.out_channels)
@@ -44,7 +45,8 @@ class CNN(nn.Module):
             out_channels=32,
             kernel_size=(3, 3),
             stride=self.stride,
-            padding=(43,21)
+            padding=(43,21),
+            bias=False
         )
         self.initialise_layer(self.conv2)
         self.batch2 = nn.BatchNorm2d(self.conv2.out_channels)
@@ -59,7 +61,8 @@ class CNN(nn.Module):
             out_channels=64,
             kernel_size=(3, 3),
             stride=self.stride,
-            padding=(22,11)
+            padding=(22,11),
+            bias=False
         )
         self.initialise_layer(self.conv3)
         self.batch3 = nn.BatchNorm2d(self.conv3.out_channels)
@@ -73,23 +76,31 @@ class CNN(nn.Module):
             out_channels=64,
             kernel_size=(3, 3),
             stride=self.stride,
-            padding=(1,1))
+            padding=(1,1), 
+            bias=False
+        )
         self.initialise_layer(self.conv4)
         self.batch4 = nn.BatchNorm2d(self.conv3.out_channels)
+
         params = sum(p.numel() for p in self.conv4.parameters() if p.requires_grad)
         print(f"Number of params layer 4: {params}")
         #self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2,2), padding=(1,1))
 
-        self.fc1 = nn.Linear(15488, 1024)
+        self.fc1 = nn.Linear(15488, 1024, bias=False)
         self.initialise_layer(self.fc1)
         params = sum(p.numel() for p in self.fc1.parameters() if p.requires_grad)
         print(f"Number of params layer 5: {params}")
 
-        self.fc2 = nn.Linear(1024, 10)
+        self.fc2 = nn.Linear(1024, 10, bias=False)
         self.initialise_layer(self.fc2)
         params = sum(p.numel() for p in self.parameters() if p.requires_grad)
         print(f"Number of params: {params}")
 
+        self.softy = nn.Softmax(dim=1)
+
+    # Batch norm should come after relu: https://www.reddit.com/r/MachineLearning/comments/67gonq/d_batch_normalization_before_or_after_relu/
+    # Pooling can come before or after activation function: https://stackoverflow.com/questions/35543428/activation-function-after-pooling-layer-or-convolutional-layer
+    # dropout: https://stackoverflow.com/questions/39691902/ordering-of-batch-normalization-and-dropout 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         x = self.conv1(images)
         x = F.relu(x)
@@ -117,13 +128,14 @@ class CNN(nn.Module):
         x = self.dropout(x)
 
         x = self.fc2(x)
-        x = F.softmax(x, dim=-1)
+        x = self.softy(x)
 
         return x 
 
     @staticmethod
     def initialise_layer(layer):
         if hasattr(layer, "bias"):
-            nn.init.zeros_(layer.bias)
+            if type(layer.bias) != type(None):
+                nn.init.zeros_(layer.bias)
         if hasattr(layer, "weight"):
             nn.init.kaiming_normal_(layer.weight)
